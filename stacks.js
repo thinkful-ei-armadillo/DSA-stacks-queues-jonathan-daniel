639c737d07c6678cf3d985c1e542b1144e9da5e9
Daniel Ors <do7811@gmail.com>
2019-04-05 10:48:14 -0400

    completed starting point

--- stacks.js
+++ stacks.js
@@ -37,6 +37,46 @@ class Stack {
     return node.data;
   }
 
-
-
 }
+
+function main() {
+  let starTrek = new Stack;
+
+  starTrek.push('Kirk');
+  starTrek.push('Spock');
+  starTrek.push('McCoy');
+  starTrek.push('Scotty');
+
+  console.log(JSON.stringify(starTrek, null, 2));
+}
+
+main();
+
+// 2. useful methods for stack
+// peek() - display top of the stack
+// isEmpty(): allows you to check if the stack is empty or not
+// display(): to display the stack - what is the 1st item in your stack?
+// Remove McCoy from your stack and display the stack
+
+
+// 3. Check for palindromes using a stack
+// A palindrome is a word, phrase, or number that is spelled the same forward and backward.
+// For example, “dad” is a palindrome; “A man, a plan, a canal: Panama” is a palindrome if
+// you take out the spaces and ignore the punctuation; and 1,001 is a numeric palindrome. 
+// We can use a stack to determine whether or not a given string is a palindrome.
+
+// Write an algorithm that uses a stack to determine whether a given input is palindrome or not. 
+// Use the following template as a starting point.
+
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  // Your code goes here
+}
+
+// True, true, true, false
+console.log(is_palindrome('dad'));
+console.log(is_palindrome('A man, a plan, a canal: Panama'));
+console.log(is_palindrome('1001'));
+console.log(is_palindrome('Tauhida'));
+
+// 
